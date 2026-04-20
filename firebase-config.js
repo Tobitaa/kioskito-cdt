dd64a49565e74c8908a7d923d5268407f4247415
CRISTOBAL VALDES <cjvaldes2@uc.cl>
2026-04-20 02:46:11 -0400

    config: agregar firebase config

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// ============================================================
+//  firebase-config.js
+//
+//  ⚠️  ESTE ARCHIVO ESTÁ EN .gitignore — NO SE SUBE A GITHUB ⚠️
+//
+//  Reemplaza los valores con tu configuración real de Firebase.
+//  Para desplegar en GitHub Pages, sube SOLO el sitio compilado
+//  (no el código fuente con este archivo).
+// ============================================================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAxMg5suEGuDji91wZ2LII-d6hwdqn3zk",
+  authDomain: "tukioskito-d3287.firebaseapp.com",
+  projectId: "tukioskito-d3287",
+  storageBucket: "tukioskito-d3287.firebasestorage.app",
+  messagingSenderId: "324478863951",
+  appId: "1:324478863951:web:03a69de9ada0c5b455783a"
+};
+
